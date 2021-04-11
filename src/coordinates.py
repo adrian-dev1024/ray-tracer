ed308bcbf503d1f62b2345b56f9f6ab930b74786
Adrian Garay <adrian.dev.1024@gmail.com>
2021-04-10 22:21:22 -0400

    added more Coordinate and Vertor func

--- src/coordinates.py
+++ src/coordinates.py
@@ -1,25 +1,93 @@
-from collections import namedtuple
-
-Coordinate = namedtuple('Coordinate', ['x', 'y', 'z', 'w'])
-
-
-def is_a_point(coordinate: Coordinate):
-    return coordinate.w == Point.w
+import math
+from dataclasses import dataclass
 
 
-def is_a_vector(coordinate: Coordinate):
-    return coordinate.w == Vector.w
+@dataclass(eq=False)
+class Coordinate:
+    x: float
+    y: float
+    z: float
+    w: float
+
+    def is_a_point(self):
+        return self.w == Point.w
+
+    def is_a_vector(self):
+        return self.w == Vector.w
+
+    def __eq__(self, other):
+        """Overrides the default implementation"""
+        if isinstance(other, Coordinate):
+            return self.x == other.x and \
+                   self.y == other.y and \
+                   self.z == other.z and \
+                   self.w == other.w
+        return NotImplemented
+
+    # TODO: Not sure what happens when adding a point to a point
+    def __add__(self, other):
+        return Coordinate(
+            self.x + other.x,
+            self.y + other.y,
+            self.z + other.z,
+            self.w + other.w
+        )
+
+    # TODO: Only works for 2 point to get a vector
+    def __sub__(self, other):
+        return Coordinate(
+            self.x - other.x,
+            self.y - other.y,
+            self.z - other.z,
+            self.w - other.w
+        )
+
+    def __neg__(self):
+        return Coordinate(0, 0, 0, 0) - self
+
+    def __mul__(self, other):
+        scalar = float(other)
+        return Coordinate(
+            self.x * scalar,
+            self.y * scalar,
+            self.z * scalar,
+            self.w * scalar
+        )
+
+    def __truediv__(self, other):
+        scalar = float(other)
+        return Coordinate(
+            self.x / scalar,
+            self.y / scalar,
+            self.z / scalar,
+            self.w / scalar
+        )
+
+@dataclass
+class Point(Coordinate):
+    w: float = 1.
 
 
-class Point:
-    w = 1.
+@dataclass
+class Vector(Coordinate):
+    w: float = 0.
 
-    def __init__(self, x, y, z):
-        self.coordinate = Coordinate(x, y, z, self.w)
+    def magnitude(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
 
+    def normalize(self):
+        magnitude = self.magnitude()
+        return Vector(
+            self.x / magnitude,
+            self.y / magnitude,
+            self.z / magnitude
+        )
 
-class Vector:
-    w = 0.
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y + self.z * other.z
 
-    def __init__(self, x, y, z):
-        self.coordinate = Coordinate(x, y, z, self.w)
+    def cross(self, other):
+        return Vector(
+            self.y * other.z - self.z * other.y,
+            self.z * other.x - self.x * other.z,
+            self.x * other.y - self.y * other.x)
