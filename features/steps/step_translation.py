ec2ce9e844473674b28fe48069ad58a7f92d6d1e
Adrian Garay <adrian.dev.1024@gmail.com>
2021-05-07 15:07:04 -0400

    add scaling

--- features/steps/step_translation.py
+++ features/steps/step_translation.py
@@ -10,34 +10,84 @@
 from behave import given, then
 
 from src.coordinates import Point, Vector
-from src.matrix import Translation
+from src.matrix import TranslationMatrix, ScalingMatrix
 
 
-@given('transform ← Translation(5, -3, 2)')
+@given('transform ← TranslationMatrix(5, -3, 2)')
 def step_impl(context):
-    context.transform = Translation(5, -3, 2)
+    context.transform = TranslationMatrix(5, -3, 2)
 
 
 @given('p ← Point(-3, 4, 5)')
 def step_impl(context):
     context.p = Point(-3, 4, 5)
 
+
 @given('inv ← transform.inverse()')
 def step_impl(context):
     context.inv = context.transform.inverse()
+
+
+@given('transform ← ScalingMatrix(2, 3, 4)')
+def step_impl(context):
+    context.transform = ScalingMatrix(2, 3, 4)
+
+
+@given('p ← Point(-4, 6, 8)')
+def step_impl(context):
+    context.p = Point(-4, 6, 8)
+
+
+@given('v ← Vector(-4, 6, 8)')
+def step_impl(context):
+    context.v = Vector(-4, 6, 8)
+
 
 @given('v ← Vector(-3, 4, 5)')
 def step_impl(context):
     context.v = Vector(-3, 4, 5)
 
+
+@given('transform ← ScalingMatrix(-1, 1, 1)')
+def step_impl(context):
+    context.transform = ScalingMatrix(-1, 1, 1)
+
+
+@given('p ← Point(2, 3, 4)')
+def step_impl(context):
+    context.p = Point(2, 3, 4)
+
+
 @then('transform * p = Point(2, 1, 7)')
 def step_impl(context):
     assert context.transform * context.p == Point(2, 1, 7)
+
 
 @then('inv * p = Point(-8, 7, 3)')
 def step_impl(context):
     assert context.inv * context.p == Point(-8, 7, 3)
 
+
 @then('transform * v = v')
 def step_impl(context):
     assert context.transform * context.v == context.v
+
+
+@then('transform * p = Point(-8, 18, 32)')
+def step_impl(context):
+    assert context.transform * context.p == Point(-8, 18, 32)
+
+
+@then('transform * v = Vector(-8, 18, 32)')
+def step_impl(context):
+    assert context.transform * context.v == Vector(-8, 18, 32)
+
+
+@then('inv * v = Vector(-2, 2, 2)')
+def step_impl(context):
+    assert context.inv * context.v == Vector(-2, 2, 2)
+
+
+@then('transform * p = Point(-2, 3, 4)')
+def step_impl(context):
+    assert context.transform * context.p == Point(-2, 3, 4)
--- features/translation.feature
+++ features/translation.feature
@@ -1,17 +1,38 @@
 Feature: Matrix Transformations
 
-  Scenario: Multiplying by a translation matrix
-    Given transform ← Translation(5, -3, 2)
+  Scenario: Multiplying by a TranslationMatrix matrix
+    Given transform ← TranslationMatrix(5, -3, 2)
     And p ← Point(-3, 4, 5)
     Then transform * p = Point(2, 1, 7)
 
-  Scenario: Multiplying by the inverse of a translation matrix
-    Given transform ← Translation(5, -3, 2)
+  Scenario: Multiplying by the inverse of a TranslationMatrix matrix
+    Given transform ← TranslationMatrix(5, -3, 2)
     And inv ← transform.inverse()
     And p ← Point(-3, 4, 5)
     Then inv * p = Point(-8, 7, 3)
 
-  Scenario: Translation does not affect vectors
-    Given transform ← Translation(5, -3, 2)
+  Scenario: TranslationMatrix does not affect vectors
+    Given transform ← TranslationMatrix(5, -3, 2)
     And v ← Vector(-3, 4, 5)
     Then transform * v = v
+
+  Scenario: A scaling matrix applied to a point
+    Given transform ← ScalingMatrix(2, 3, 4)
+    And p ← Point(-4, 6, 8)
+    Then transform * p = Point(-8, 18, 32)
+
+  Scenario: A scaling matrix applied to a vector
+    Given transform ← ScalingMatrix(2, 3, 4)
+    And v ← Vector(-4, 6, 8)
+    Then transform * v = Vector(-8, 18, 32)
+
+  Scenario: Multiplying by the inverse of a scaling matrix
+    Given transform ← ScalingMatrix(2, 3, 4)
+    And inv ← transform.inverse()
+    And v ← Vector(-4, 6, 8)
+    Then inv * v = Vector(-2, 2, 2)
+
+  Scenario: Reflection is scaling by a negative value
+    Given transform ← ScalingMatrix(-1, 1, 1)
+    And p ← Point(2, 3, 4)
+    Then transform * p = Point(-2, 3, 4)
