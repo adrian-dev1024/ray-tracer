c30d6395deacfeb8624edf4c0000a963c85a8adb
Adrian Garay <adrian.dev.1024@gmail.com>
2021-05-18 21:47:58 -0400

    add sphere intersect

---
+++ features/steps/step_sphere.py
@@ -0,0 +1,99 @@
+"""
+
+"""
+
+# @mark.steps
+# ----------------------------------------------------------------------------
+# STEPS:
+# ----------------------------------------------------------------------------
+
+from behave import given, then, when
+
+from src.matrix import Point, Vector
+from src.ray import Ray
+from src.sphere import Sphere
+
+
+@given('r ← Ray(Point(0, 0, -5), Vector(0, 0, 1))')
+def step_impl(context):
+    context.r = Ray(Point(0, 0, -5), Vector(0, 0, 1))
+
+
+@given('r ← Ray(Point(0, 1, -5), Vector(0, 0, 1))')
+def step_impl(context):
+    context.r = Ray(Point(0, 1, -5), Vector(0, 0, 1))
+
+
+@given('r ← Ray(Point(0, 2, -5), Vector(0, 0, 1))')
+def step_impl(context):
+    context.r = Ray(Point(0, 2, -5), Vector(0, 0, 1))
+
+
+@given('r ← Ray(Point(0, 0, 0), Vector(0, 0, 1))')
+def step_impl(context):
+    context.r = Ray(Point(0, 0, 0), Vector(0, 0, 1))
+
+
+@given('r ← Ray(Point(0, 0, 5), Vector(0, 0, 1))')
+def step_impl(context):
+    context.r = Ray(Point(0, 0, 5), Vector(0, 0, 1))
+
+
+@given('s ← Sphere()')
+def step_impl(context):
+    context.s = Sphere()
+
+
+@when('xs ← s.intersect(r)')
+def step_impl(context):
+    context.xs = context.s.intersect(context.r)
+
+
+@then('len(xs) = 2')
+def step_impl(context):
+    assert len(context.xs) == 2
+
+
+@then('len(xs) = 0')
+def step_impl(context):
+    assert len(context.xs) == 0
+
+
+@then('xs[0] = 4.0')
+def step_impl(context):
+    assert context.xs[0] == 4.0
+
+
+@then('xs[1] = 6.0')
+def step_impl(context):
+    assert context.xs[1] == 6.0
+
+
+@then('xs[0] = 5.0')
+def step_impl(context):
+    assert context.xs[0] == 5.0
+
+
+@then('xs[1] = 5.0')
+def step_impl(context):
+    assert context.xs[1] == 5.0
+
+
+@then('xs[0] = -1.0')
+def step_impl(context):
+    assert context.xs[0] == -1.0
+
+
+@then('xs[1] = 1.0')
+def step_impl(context):
+    assert context.xs[1] == 1.0
+
+
+@then('xs[0] = -6.0')
+def step_impl(context):
+    assert context.xs[0] == -6.0
+
+
+@then('xs[1] = -4.0')
+def step_impl(context):
+    assert context.xs[1] == -4.0
